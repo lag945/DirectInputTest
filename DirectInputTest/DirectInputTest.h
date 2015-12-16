359a1b8bbd149900c1f64bda8002ef2cd89d6498
拉狗那 <lag945@hotmail.com>
2015-12-16 14:51:41 +0800

    blank

---
+++ DirectInputTest/DirectInputTest.h
@@ -0,0 +1,35 @@
+
+// DirectInputTest.h : main header file for the DirectInputTest application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CDirectInputTestApp:
+// See DirectInputTest.cpp for the implementation of this class
+//
+
+class CDirectInputTestApp : public CWinApp
+{
+public:
+	CDirectInputTestApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CDirectInputTestApp theApp;
